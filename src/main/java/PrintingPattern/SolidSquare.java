bf120e0bc13101e1d5dee17bbeacfe7055ed41c4
Mrutyunjaya Sahoo <sahoo10022004@gmail.com>
2026-09-04 21:05:37 +0530

    SolidSquare

---
+++ src/main/java/PrintingPattern/SolidSquare.java
@@ -0,0 +1,27 @@
+package PrintingPattern;
+
+import java.util.Scanner;
+
+//        *****
+//        *****
+//        *****
+//        *****
+//        *****
+public class SolidSquare {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n;
+        System.out.print("Enter a nwe number :");
+        n=sc.nextInt();
+
+        for(int i=1; i<= n ;i++)
+        {
+            for(int j=1; j<= n; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
