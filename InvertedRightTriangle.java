940a738fe52758f505bfcfe7974f31659af1df0a
Mrutyunjaya Sahoo <sahoo10022004@gmail.com>
2026-09-06 23:26:38 +0530

    Implement inverted right triangle pattern in main
    Fix the class name and implement the inverted right triangle pattern.

---
+++ InvertedRightTriangle.java
@@ -0,0 +1,14 @@
+public class main{
+public static void main(String[] args)
+  {
+    int num=5;
+    for(int i=1; i<=n; i++)
+      {
+        for(int j=1; j<n-i+1; j++)
+          {
+            System.out.print("*");
+          }//inner loop
+        System.out.println();
+      }
+  }
+}
